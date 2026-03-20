241ab6a694e0c7df9a5e8a1adb94cfd12b22134a
Michael P. <polmichael552@gmail.com>
2026-03-20 17:45:28 +0200

    feat: Introduce Othello backend to generate training data and train a Keras CNN model.

--- backend-othello/main.py
+++ backend-othello/main.py
@@ -4,7 +4,7 @@ from tournament import Tournament
 def main():
     t = SmartTournament(size=6, discount=0.9)
     #t = Tournament(size=6, discount=0.9)
-    t.play_games(50_000)
+    t.play_games(10_000) # real games for full games list = 50_000
     t.export_csv("othello_training_data.csv")
 
 if __name__ == "__main__":
